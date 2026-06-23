feba7d640a0f802e57b1ad7c31a3b3acdeb8fefe
srkarmakar <shirshankar1947@gmail.com>
2026-06-23 12:51:58 +0530

    UI Code - Incomplete

---
+++ src/app/feature/task-item/task-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  imports: [],
+  templateUrl: './task-item.html',
+  styleUrl: './task-item.scss',
+})
+export class TaskItem {}
